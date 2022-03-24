04fbc513f372d193b121ad8ac5bf986ac0c3d9ca
Bigyan Darshan Patel <bigyanpatel07769@gmail.com>
2022-03-25 03:45:31 +0530

    fix: extracted all url links by scraping

--- web scraping/main.js
+++ web scraping/main.js
@@ -8,35 +8,36 @@ const cheerio = require("cheerio");
 // home page -> all mathces -> find out all urls -> details -> check folders of player name ->
 
 // home page
-request(url, cb);
-function cb (err, response, html){
-    if(err){
-        console.log(err);
-    } else {
-        // console.log(html);
-        extractLink(html);
-    }
-}
+request(url, (err, response, html) => {
+        if (err) {
+            console.log(err);
+        } else {
+            // console.log(html);
+            extractLink(html);
+        }
+    });
+
 function extractLink(html){
     let $ = cheerio.load(html);
     let anchorElem = $("a[data-hover='View All Results']");
     let link = anchorElem.attr("href");
     // console.log(link);
-    let fullLink = "www.espncricinfo.com" + link;
+    let fullLink = "https://www.espncricinfo.com" + link;
     // console.log(fullLink);
     getAllMatchesLink(fullLink);
 
 }
 
 function getAllMatchesLink(url){
-    request(url, function(err, response, html){
-        if(err){
-            console.log(err);
-        } else{
-            // console.log(html);
-            extractAllLinks(html);
-        }
-    })
+    request(url, (err, response, html) => {
+            if (err) {
+                console.log(err);
+            } else {
+                // console.log(html);
+                extractAllLinks(html);
+            }
+        });
+    
 }
 
 function extractAllLinks(html){
@@ -47,21 +48,3 @@ function extractAllLinks(html){
         console.log(link);
     }
 }
-// function getAllMathcesLink(url){
-//     request(url, function(err, response, html){
-//         if(err){
-//             console.log(err);
-//         } else {
-//             // console.log(html);
-//             extractAllLinks(html);
-//         }
-//     })
-// }
-// function extractAllLinks(html){
-//     let $ = cheerio.load(html);
-//     let scorecardElems = $("a[data-hover='Scorecard']");
-//     for(let i = 0; i < scorecardElems.length; i++){
-//         let link = $(scorecardElems[i]).attr("href");
-//         console.log(link);
-//     }
-// }
