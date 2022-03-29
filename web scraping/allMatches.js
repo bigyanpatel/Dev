6ae109428fd56250683d8fc3abf664f6d6690bbf
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-03-30 03:07:53 +0530

    update : learned json and xlsx

---
+++ web scraping/allMatches.js
@@ -0,0 +1,29 @@
+const request = require("request");
+const cheerio = require("cheerio");
+const scorecardObj = require("./scorecard")
+
+function getAllMatchesLink(url){
+    request(url, (err, response, html) => {
+            if (err) {
+                console.log(err);
+            } else {
+                // console.log(html);
+                extractAllLinks(html);
+            }
+        });
+    
+}
+function extractAllLinks(html){
+    let $ = cheerio.load(html);
+    let scorecardElems = $("a[data-hover='Scorecard']");
+    for(let i = 0; i < scorecardElems.length; i++){
+        let link = $(scorecardElems[i]).attr("href");
+        let fullLink = "https://www.espncricinfo.com" + link;
+        // console.log(fullLink);
+        scorecardObj.ps(fullLink);
+    }
+}
+
+module.exports = {
+    gAllMathces : getAllMatchesLink
+}
