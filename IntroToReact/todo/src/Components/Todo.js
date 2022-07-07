5679ab9c41ba5fee2d03b624696e5171d90c9f60
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-07-07 20:43:33 +0530

    completed with the basic todo app

--- IntroToReact/todo/src/Components/Todo.js
+++ IntroToReact/todo/src/Components/Todo.js
@@ -9,16 +9,38 @@ class Todo extends Component {
         }
     }
 
+    handleChange = (e) => {
+        console.log(e.target.value)
+        this.setState({
+            currTask: e.target.value
+        })
+    }
+
+    handleSubmit = () => {
+        this.setState({
+            tasks:[...this.state.tasks,{task:this.state.currTask,id:this.state.tasks.length + 1}]
+        })
+    }
+
+    handleDelete = (id) => {
+        let narr = this.state.tasks.filter((taskObj) => {
+            return taskObj.id !== id
+        })
+        this.setState({
+            tasks: [...narr]
+        })
+    }
+
     render(){
         return (
             <ul>
-                <input type= "text"/>
-                <button>Submit</button>
+                <input type= "text" value ={this.state.currTask} onChange={this.handleChange}/>
+                <button onClick={this.handleSubmit}>Submit</button>
                 {
                     this.state.tasks.map((taskObj) => (
                         <li key = {taskObj.id}>
                             <p>{taskObj.task}</p>
-                            <button>Delete</button>
+                            <button onClick={() => this.handleDelete(taskObj.id)}>Delete</button>
                         </li>
                     ))
                 }
