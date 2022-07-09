8bcfd13982259535b59f34625c0101f27a04d129
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-07-09 12:00:20 +0530

    data called from api and implemented pagination

--- IntroToReact/movie/src/Components/Movies.js
+++ IntroToReact/movie/src/Components/Movies.js
@@ -1,8 +1,8 @@
 import React, { Component } from 'react'
-import { movies } from './getMovies'
+// import { movies } from './getMovies'
+import axios from 'axios';
 
 export default class Movies extends Component {
-  render() {
     constructor(){
         super();
         this.state={
@@ -13,10 +13,58 @@ export default class Movies extends Component {
             favourites:[]
         }
     }
+
+    async componentDidMount(){
+        //Side effects 
+        const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=${this.state.currPage}`);
+        let data = res.data
+        // console.log(data);
+        this.setState({
+            movies:[...data.results]
+        })
+    }
+
+    changeMovies=async()=>{
+        console.log("changemovies called");
+        console.log(this.state.currPage);
+        const res = await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=5540e483a20e0b20354dabc2d66a31c9&language=en-US&page=${this.state.currPage}`);
+        let data = res.data
+        // console.log(data);
+        this.setState({
+            movies:[...data.results]
+        })
+    }
+    handleRight=()=>{
+        let temparr =[]
+        for(let i=1;i<=this.state.parr.length+1;i++){
+            temparr.push(i);
+        }
+        this.setState({
+            parr:[...temparr],
+            currPage:this.state.currPage+1
+        },this.changeMovies)
+    }
+    handleLeft=()=>{
+        if(this.state.currPage!=1){
+            this.setState({
+                currPage:this.state.currPage-1
+            },this.changeMovies)
+        }
+    }
+    handleClick=(value)=>{
+        if(value!=this.state.currPage){
+            console.log("i am called")
+            this.setState({
+                currPage:value
+            },this.changeMovies)
+        }
+    }
+
+    render() {
     return (
       <>
       {
-        movies.length === 0 ?
+        this.state.movies.length === 0 ?
         <div class="spinner-border text-primary" role="status">
             <span class="visually-hidden">Loading...</span>
         </div> :
@@ -42,11 +90,8 @@ export default class Movies extends Component {
                     ))
                 }
             </div>
-            <div className="infinite-loader"style={{display:'flex',justifyContent:'center'}}>
-                <h2>Load More Movies .........................</h2>
-            </div>
-            {/* <div style={{display:'flex',justifyContent:'center'}}> */}
-            {/* <nav aria-label="Page navigation example">
+            <div style={{display:'flex',justifyContent:'center'}}>
+            <nav aria-label="Page navigation example">
                 <ul class="pagination">
                     <li class="page-item"><a class="page-link" onClick={this.handleLeft}>Previous</a></li>
                     {
@@ -56,8 +101,8 @@ export default class Movies extends Component {
                     }
                     <li class="page-item"><a class="page-link" onClick={this.handleRight}>Next</a></li>
                 </ul>
-            </nav> */}
-            {/* </div> */}
+            </nav>
+            </div>
         </div>
       }
       </>
