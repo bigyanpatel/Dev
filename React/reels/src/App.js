8445574e5b7d5bbc9a10aed336ecb31d7759b1e7
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-07-12 15:25:35 +0530

    done with basic structuring of Signup page

---
+++ React/reels/src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import Signup from './Components/Signup'
+import {BrowserRouter} from 'react-router-dom'
+
+function App() {
+  return (
+    <BrowserRouter>
+      <Signup/>
+    </BrowserRouter>
+    
+  );
+}
+
+export default App;
