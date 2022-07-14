0efc9660f56e53b27968acef48dab5e26d00a196
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-07-14 16:20:49 +0530

    added: uploading video into our storage

--- React/reels/src/Components/feed.js
+++ React/reels/src/Components/feed.js
@@ -1,13 +1,24 @@
-import React, {useContext} from 'react'
+import React, {useContext, useState, useEffect} from 'react'
 import { AuthContext } from '../Context/AuthContext'
+import { database } from '../firebase'
+import UploadFile from './UploadFile'
 
 function Feed() {
-  const {logout} = useContext(AuthContext)
+  const {user,logout} = useContext(AuthContext)
+    const [userData,setUserData] = useState('')
+    useEffect(()=>{
+        const unsub = database.users.doc(user.uid).onSnapshot((snapshot)=>{
+            setUserData(snapshot.data())
+        })
+        return ()=> {unsub()}
+    },[user])
   return (
-    <div>
-      <h1>Welcome to feeeed!</h1>
-      <button onClick={logout}>Logout</button>
-        
+    <div style={{display:'flex',justifyContent:'center',alignItems:'center',flexDirection:'column'}}>
+      <div className="comp" style={{width:'50%'}}>
+          <h1>Welcome to feed</h1>
+          <button onClick={logout}>Log out</button>
+          <UploadFile user = {userData}/>
+      </div>
     </div>
   )
 }
