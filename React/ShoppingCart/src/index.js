c67e736e60181bffcf61e53dd912952e330f6401
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-07-22 15:43:31 +0530

    implemented redux for the cart

--- React/ShoppingCart/src/index.js
+++ React/ShoppingCart/src/index.js
@@ -3,12 +3,16 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import * as serviceWorker from "./serviceWorker";
+import { Provider } from "react-redux";
+import store from './redux/store'
 
 // Redux
-import { Provider } from "react-redux";
+// import { Provider } from "react-redux";
 
 ReactDOM.render(
-    <App />,
+  <Provider store={store}>
+    <App />
+  </Provider>,
   document.getElementById("root")
 );
 
