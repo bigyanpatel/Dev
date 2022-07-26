86695cac9f67e6ecad8a4882a5599bac69ef9149
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-07-26 21:17:52 +0530

    build deployed site

---
+++ React/ShoppingCart/build/precache-manifest.f3c0f6f60a6075cd50888fde76b12a98.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9c044c28379b4d2e3960557b60a5ff92",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6b3caee57edc3173cb66",
+    "url": "/static/css/main.a5a9ddbf.chunk.css"
+  },
+  {
+    "revision": "32b650c25670705f154e",
+    "url": "/static/js/2.7124a6e1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7124a6e1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6b3caee57edc3173cb66",
+    "url": "/static/js/main.df367daa.chunk.js"
+  },
+  {
+    "revision": "b10d08823a4df677ba67",
+    "url": "/static/js/runtime-main.de931e19.js"
+  },
+  {
+    "revision": "028978f40ca009a385c6fb19486bdc53",
+    "url": "/static/media/book.028978f4.jpg"
+  },
+  {
+    "revision": "67a487675e594f3b274f76d190aa011f",
+    "url": "/static/media/smartphone.67a48767.jpg"
+  },
+  {
+    "revision": "4a0dbe85527b88bfcea004afbf6c9fb8",
+    "url": "/static/media/speaker.4a0dbe85.jpg"
+  }
+]);
