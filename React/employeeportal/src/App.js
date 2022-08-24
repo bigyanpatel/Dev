0bc523d6ea2e8f9c55040db9362079a1156367a2
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-08-24 19:16:56 +0530

    add: initial commit of app

---
+++ React/employeeportal/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {ToastContainer} from "react-toastify";
+import './App.css';
+import auth from './Components/auth';
+i
+
+function App() {
+  return (
+    <div className='App'>
+      <ToastContainer/>
+      <h1>Hello</h1>
+    </div>
+  );
+}
+
+export default App;
