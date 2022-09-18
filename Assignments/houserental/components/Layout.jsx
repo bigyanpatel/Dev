bb6ab699dd7cc4f54996afe532f9f98695b61658
Bigyan Darshan Patel <86798277+bigyanpatel@users.noreply.github.com>
2022-09-18 22:14:23 +0530

    feat: implemented navbar

--- Assignments/houserental/components/Layout.jsx
+++ Assignments/houserental/components/Layout.jsx
@@ -1,6 +1,8 @@
 import Head from 'next/head';
 import { Box } from '@chakra-ui/react';
 
+import Navbar from './Navbar';
+
 
 export default function Layout({ children }) {
   return (
@@ -10,7 +12,7 @@ export default function Layout({ children }) {
       </Head>
       <Box maxWidth='1280px' m='auto'>
         <header>
-          Navbar
+          <Navbar/>
         </header>
         <main>{children}</main>
         <footer>
